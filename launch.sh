f32e83a0547904afd24ec22c3a734593df182ece
Parthrajgohil <179858018+parthraj-g0hil@users.noreply.github.com>
2025-06-26 15:19:11 +0530

    Delete launch.sh

--- launch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-exec java -Djava.util.logging.config.file=/etc/jitsi/jibri/logging.properties \
-    -Dconfig.file="/etc/jitsi/jibri/jibri.conf" \
-    -jar /opt/jitsi/jibri/jibri.jar
