3f2b58a3208920266bb631e5807c1ee1bf8a93af
Parthrajgohil <179858018+parthraj-g0hil@users.noreply.github.com>
2025-06-26 15:19:44 +0530

    Add files via upload

---
+++ jibri/launch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+exec java -Djava.util.logging.config.file=/etc/jitsi/jibri/logging.properties \
+    -Dconfig.file="/etc/jitsi/jibri/jibri.conf" \
+    -jar /opt/jitsi/jibri/jibri.jar
